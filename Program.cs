4c8b9184f13d5404e1e602c05129cbe8c0f73295
2016lcs <48083553+2016lcs@users.noreply.github.com>
2019-02-28 21:42:05 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace work2
+{
+    public class Form1 : Form
+    {
+        double a,b,c;//定义即将输入的数字以及结果
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c = a * b;
+            label4.Text = Convert.ToString(c);
+        }
+    }
+}
