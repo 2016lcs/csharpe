649b8b97265d2730b979e844c495eb1aa96e6413
2016lcs <48083553+2016lcs@users.noreply.github.com>
2019-02-28 22:00:38 +0800

    Add files via upload

---
+++ work 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "";
+            double a,b,c;//定义两个将要输入的数字及其乘积
+            Console.WriteLine("intput first date:");
+            s = Console.ReadLine();
+            a = double.Parse(s);
+            Console.WriteLine("intput second date:");
+            s = Console.ReadLine();
+            b = double.Parse(s);
+            c = a * b;
+            Console.WriteLine($"乘积：{a}*{b}={c}");
+        }
+    }
+}
